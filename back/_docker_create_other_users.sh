fa0041e9182511245240325a787f28a8876d5ea4
paoDweb <info@paoDweb.com>
2022-03-02 16:22:51 +0100

    feat: assign users to folders

---
+++ back/_docker_create_other_users.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+CONTAINER_ID=`docker ps -aqf "name=pialab_back"`
+
+# applications
+
+docker exec -it $CONTAINER_ID bin/console pia:application:create --name=Default-app --url=
+
+# structures
+
+docker exec -it $CONTAINER_ID bin/console pia:structure:create --name=Default-struct --type=default-S1-type
+
+# users
+
+docker exec -it $CONTAINER_ID bin/console pia:user:create Emmanuel@Eval.Eval Emmanuel \
+   --application=Default-app --structure=Default-struct
+
+docker exec -it $CONTAINER_ID bin/console pia:user:promote Emmanuel@Eval.Eval --role=ROLE_EVALUATOR
+
+
+docker exec -it $CONTAINER_ID bin/console pia:user:create Robert@Red.Red Robert \
+   --application=Default-app --structure=Default-struct
+
+docker exec -it $CONTAINER_ID bin/console pia:user:promote Robert@Red.Red --role=ROLE_REDACTOR
+
+
+docker exec -it $CONTAINER_ID bin/console pia:user:create Daniel@DPO.DPO Daniel \
+   --application=Default-app --structure=Default-struct
+
+docker exec -it $CONTAINER_ID bin/console pia:user:promote Daniel@DPO.DPO --role=ROLE_DPO
