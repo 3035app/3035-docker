f941a6a454896d0948ceca8b72ea99079040fe01
Jean-Philippe Ouvrard <jp.ouvrard@lesoctetslibres.com>
2022-03-02 15:30:35 +0100

    chore: :construction_worker: Move postgresql cmd + db init from doskerfile to entrypoint

--- back/docker/entrypoint.sh
+++ back/docker/entrypoint.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 /etc/init.d/postgresql start
+echo "CREATE ROLE user_pialab SUPERUSER LOGIN PASSWORD 'password_pialab'" | sudo -u postgres psql template1 \
+    && cd /var/www/pialab-back/ \
+    && bin/console doctrine:database:create \
+    && bin/console doctrine:migrations:migrate --no-interaction \
+    && bin/console pia:user:create test@test.tld test --username=test \
+    && bin/console pia:user:promote test@test.tld --role=ROLE_SUPER_ADMIN \
+    && bin/console pia:user:promote test@test.tld --role=ROLE_REDACTOR \
+    && bin/console pia:user:promote test@test.tld --role=ROLE_EVALUATOR \
+    && bin/console pia:user:promote test@test.tld --role=ROLE_CONTROLLER \
+    && bin/console pia:user:promote test@test.tld --role=ROLE_CONTROLLER_MULTI \
+    && bin/console pia:user:promote test@test.tld --role=ROLE_DPO \
+    && bin/console pia:user:promote test@test.tld --role=ROLE_SHARED_DPO
 $@
