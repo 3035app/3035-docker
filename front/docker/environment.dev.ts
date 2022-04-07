6d090026a619c07a0124a3c201ae0df90de29f7d
Jean-Philippe Ouvrard <jp.ouvrard@lesoctetslibres.com>
2022-04-07 17:20:15 +0200

    feat: :sparkles: Add tenant property to front environment files

--- front/docker/environment.dev.ts
+++ front/docker/environment.dev.ts
@@ -12,7 +12,8 @@ export const environment = {
   api: {
     client_id:     '4lwqvlm61aww88scgc0ok0w888gc0co8g4s4swkck0wwwocgw0',
     client_secret: '34b4ht6mo2kggoww0ko4c040cww0g0c8k0gcskg0w4wgkkcks4',
-    host:          'http://back',
+    host:          'http://back.pialab.localhost',
     token_path:    '/oauth/v2/token'
   },
+  tenant: 'sncf'
 };
